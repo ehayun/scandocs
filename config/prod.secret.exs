edb6e27b594626f3efcf8b731ffc217bd3201d21
Eli Hayun <eli@venus.home>
2020-05-05 11:16:24 +0300

    add secret

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -25,7 +25,7 @@ secret_key_base =
 
 config :scandoc, ScandocWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
+    port: String.to_integer(System.get_env("PORT") || "9090"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
