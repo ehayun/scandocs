cca78a104346efe71c22aa26e3b37eaca92387a1
Eli Hayun <eli@venus.home>
2020-06-11 09:13:50 +0300

    add search to permissions

--- lib/scandoc_web/live/permission_live/index.ex
+++ lib/scandoc_web/live/permission_live/index.ex
@@ -6,14 +6,17 @@ defmodule ScandocWeb.PermissionLive.Index do
 
   alias Scandoc.Permissions
   alias Scandoc.Permissions.Permission
+  alias Scandoc.Accounts.User
+
 
   @impl true
   def mount(_params, _session, socket) do
+    socket = assign(socket, search: "")
     {
       :ok,
       socket
       |> assign(:startEdit, 0)
-      |> assign(:permissions, fetch_permissions())
+      |> assign(:permissions, fetch_permissions(socket))
     }
   end
 
@@ -46,20 +49,36 @@ defmodule ScandocWeb.PermissionLive.Index do
   end
 
   @impl true
+  def handle_event("search", %{"search" => search}, socket) do
+    socket = assign(socket, search: search)
+    {:noreply, assign(socket, :permissions, fetch_permissions(socket))}
+  end
+
+  @impl true
   def handle_event("delete", %{"id" => id}, socket) do
     permission = Permissions.get_permission!(id)
     {:ok, _} = Permissions.delete_permission(permission)
 
-    {:noreply, assign(socket, :permissions, fetch_permissions())}
+    {:noreply, assign(socket, :permissions, fetch_permissions(socket))}
   end
 
   def handle_event("startEdit", %{"id" => id}, socket) do
     {:noreply, assign(socket, :startEdit, String.to_integer(id))}
   end
 
-  defp fetch_permissions do
-    # Permissions.list_permissions()
-    Permission
+  defp fetch_permissions(socket) do
+    search = socket.assigns.search
+    users = from(
+              u in User,
+              where: ilike(u.full_name, ^"%#{search}%"),
+              or_where: like(u.zehut, ^"%#{search}%"),
+              select: u.id
+            )
+            |> Repo.all
+
+    q = from(p in Permission, where: (p.user_id in ^users))
+
+    q
     |> preload(:user)
     |> order_by(:user_id)
     |> Repo.all()
