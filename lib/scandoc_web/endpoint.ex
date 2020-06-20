177384ee28ab8aefafe89062a08e9f8889b0db14
Eli Hayun <eli@venus.home>
2020-06-20 04:43:07 +0300

    do not allow to display pdf from the main site

--- lib/scandoc_web/endpoint.ex
+++ lib/scandoc_web/endpoint.ex
@@ -10,31 +10,38 @@ defmodule ScandocWeb.Endpoint do
     signing_salt: "WA2IwQLF"
   ]
 
-  socket "/socket", ScandocWeb.UserSocket,
-    websocket: true,
-    longpoll: false
+  socket "/socket",
+         ScandocWeb.UserSocket,
+         websocket: true,
+         longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/live",
+         Phoenix.LiveView.Socket,
+         websocket: [
+           connect_info: [
+             session: @session_options
+           ]
+         ]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/",
-    from: :scandoc,
-    gzip: false,
-    only: ~w(css fonts images js uploads downloads png favicon.ico robots.txt)
+       at: "/",
+       from: :scandoc,
+       gzip: false,
+       only: ~w(css fonts images js  downloads png favicon.ico robots.txt)
 
-#  plug Plug.Static,
-#    at: "/uploads",
-#    ## "./uploads", #Application.get_env(:scandoc, :full_upload_path),
-#    from: :scandoc,
-#    headers: %{"secret_header" => "1980"},
-#    gzip: false
+         #  plug Plug.Static,
+         #    at: "/uploads",
+         #    ## "./uploads", #Application.get_env(:scandoc, :full_upload_path),
+         #    from: :scandoc,
+         #    headers: %{"secret_header" => "1980"},
+         #    gzip: false
 
-  # Code reloading can be explicitly enabled under the
-  # :code_reloader configuration of your endpoint.
+         # Code reloading can be explicitly enabled under the
+         # :code_reloader configuration of your endpoint.
   if code_reloading? do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
@@ -43,16 +50,16 @@ defmodule ScandocWeb.Endpoint do
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
-    param_key: "request_logger",
-    cookie_key: "request_logger"
+       param_key: "request_logger",
+       cookie_key: "request_logger"
 
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
-    parsers: [:urlencoded, :multipart, :json],
-    pass: ["*/*"],
-    json_decoder: Phoenix.json_library()
+       parsers: [:urlencoded, :multipart, :json],
+       pass: ["*/*"],
+       json_decoder: Phoenix.json_library()
 
   plug Plug.MethodOverride
   plug Plug.Head
